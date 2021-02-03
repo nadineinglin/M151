5b91ae1c65cf8c60649a9a7adf9b9a6276d5846d
Nadine <nadine.inglin@gmail.com>
2021-02-03 23:34:48 +0100

    First lesson

---
+++ Uebungen/Uebung1-4/Uebung1-4.md
@@ -0,0 +1,6 @@
+# Uebung 1-4
+
+## Wie kann ich age auch noch mit ausgeben
+Habe es leider nicht herausgefunden
+
+
---
+++ Uebungen/Uebung1-4/index.php
@@ -0,0 +1,10 @@
+<?php
+    $username = $_GET['username'];
+    
+    $x= $_GET['x'];
+    $y= $_GET['y'];
+    $state = $_GET['state'];
+
+
+    }
+?>
